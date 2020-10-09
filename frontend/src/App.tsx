0899eebf7be2d12425daf4200cf5cd8e6c34d475
Thomas Léost <hello@urbentom.io>
2020-10-09 15:33:03 +0100

    Add notes being loaded in from api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { AxiosResponse } from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NotePreview from './components/note-preview';
+import Request from './helpers/request';
+interface Note {
+	_id: string,
+	title: string,
+	content: string
 }
 
-export default App;
+type AppProps = {}
+
+type AppState = {
+	notes: Note[]
+}
+
+export default class App extends Component< AppProps, AppState> {
+
+	constructor(props:AppProps){
+
+		super(props)
+
+		this.state = {
+			notes: []
+		}
+
+	}
+
+	componentDidMount(){
+
+		const request = new Request('http://localhost:3001')
+		const getNotes = 
+			{
+				"query": "{ notes { _id title content } }"
+			}
+
+
+
+		request.query(getNotes)
+			.then( (res:AxiosResponse) => {
+
+				this.setState({
+					notes : res.data.data.notes
+				})
+
+			})
+			.catch( (err) => {
+				console.log('err', err)
+			})
+
+
+	}
+
+	render(){
+
+		return (
+			<div>
+
+				{
+					this.state.notes.map( (note:Note) => {
+
+						return ( <NotePreview title={note.title} content={note.content} /> )
+
+					})	
+				}
+
+			</div>
+		)
+
+	}
+
+}
+
