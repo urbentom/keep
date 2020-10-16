25f090906d374dfc029ed5ea113cc52b4de72b6b
Thomas Léost <hello@urbentom.io>
2020-10-16 10:57:51 +0100

    Add popup component

---
+++ frontend/src/components/popup.tsx
@@ -0,0 +1,107 @@
+import React, { Component, MouseEvent } from "react";
+import styled from "styled-components";
+
+import ComponentButton from './buttons';
+
+import { Popup } from '../helpers/interfaces';
+
+type Props = Popup
+
+type State = {
+    isVisible: boolean
+}
+
+export default class ComponentPopup extends Component< Props, State> {
+
+    static defaultProps = {
+        title: 'Test Title',
+        content: 'I have some words here so we will write all of these words out and see what they look like.',
+        buttons: [{
+            content: 'Delete',
+            primary: true,
+            onClick: (e:MouseEvent) => {console.log('Click Delete')}
+        },
+        {
+            content: 'Cancel',
+            primary: false,
+            onClick: (e:MouseEvent) => {console.log('Click Cancel')}
+        }]
+    }
+
+    constructor(props:Props){
+
+        super(props);
+
+        this.state = {
+            isVisible: false,
+        }
+
+    }
+
+    PopupContainer = styled.div`
+        position: fixed;
+        z-index: 500000;
+        top: 0;
+        left: 0;
+        width: 100vw;
+	    height: 100vh;
+        background-color: rgba(44,36,28, 0.5);
+    `
+
+    PopupDialog = styled.div`
+        padding: 24px;
+        width: 100%;
+        max-width: 500px;
+        background-color: #eff1f7;
+        border-radius: 12px;
+        margin: auto;
+        margin-top: 50vh;
+    	transform: translateY(-50%);
+    `
+
+    PopupH2 = styled.h2`
+        margin: 0 0 1em 0;
+        font-size: 1.2em;
+        color: #2c241c;
+        font-weight: 500;
+    `
+
+    PopupP = styled.p`
+        margin: 0;
+        font-size: 1em;
+        color: #2c241c;
+        font-weight: 300;
+        line-height: 1.4em;
+        margin-bottom: 2em;
+    `
+
+    PopupButtonsContainer = styled.div`
+        text-align: right;
+    `
+
+    render(){
+
+        if(this.state.isVisible){
+
+            return (
+                <this.PopupContainer>
+                    <this.PopupDialog>
+                        <this.PopupH2>{this.props.title}</this.PopupH2>
+                        <this.PopupP>{this.props.content}</this.PopupP>
+                        <this.PopupButtonsContainer>
+                            {
+                                this.props.buttons.map( (button) => {
+                                    return ( <ComponentButton {...button} /> )
+                                })
+                            }
+                        </this.PopupButtonsContainer>
+                    </this.PopupDialog>
+                </this.PopupContainer>
+            )
+
+        }
+        else {
+            return (null);
+        }
+    }
+} 
